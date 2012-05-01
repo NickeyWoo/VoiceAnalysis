1167c90265256a46cd0d2d825757bdbde85e77f4
Nickey Woo <thenickey@gmail.com>
2012-05-01 11:47:44 +0800

    add sound diagram 2012/05/01

---
+++ src/sounddiagram_main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sndfile.h>
+
+int main(int argc, char* argv[])
+{
+	SF_INFO sndinfo;
+
+	return 0;
+}
+
